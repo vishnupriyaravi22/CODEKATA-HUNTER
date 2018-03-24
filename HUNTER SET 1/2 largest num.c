d71330874610f731e6b7779c4bef099097c42c52
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-24 10:11:01 -0700

    Create 2 largest num.c

---
+++ HUNTER SET 1/2 largest num.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,a[100];
+    int count=0,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+  
+   
+      
+
+int temp =0;
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i] < a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+               
+            }
+        }
+
+    }
+    
+     for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+  
+  
+  
+   
+    
+
+    return 0;
+}
